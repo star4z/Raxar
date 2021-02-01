e4f209726ad8fe5dec5ef77b762f11838e1645a9
Benjamin Phillips <bdphillips013@gmail.com>
2021-02-01 00:05:16 -0500

    Adds visualization of node with background and title

--- app/src/main/java/com/example/raxar/view/graph/GraphView.kt
+++ app/src/main/java/com/example/raxar/view/graph/GraphView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.Rect
 import android.util.AttributeSet
 import android.view.View
 import com.example.raxar.data.NoteDto
@@ -12,6 +13,7 @@ class GraphView : View {
 
     private val paint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
     private val graph: Graph
+    private val textBounds = Rect()
 
     constructor(context: Context?) : this(context, null, 0, 0)
     constructor(context: Context?, attrs: AttributeSet?) : this(context, attrs, 0, 0)
@@ -34,6 +36,8 @@ class GraphView : View {
         graph = Graph((1..20).map { NoteDto(title = "Title $it") })
     }
 
+    private val nodeTextSize = 40f
+
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
 
@@ -41,11 +45,27 @@ class GraphView : View {
             graph.update(width, height)
 
             for (node in graph) {
+                paint.color = Color.RED
                 canvas.drawLine(
                     graph.origin.xPos.toFloat(), graph.origin.yPos.toFloat(),
                     node.xPos.toFloat(), node.yPos.toFloat(),
                     paint
                 )
+
+                canvas.drawCircle(node.xPos.toFloat(), node.yPos.toFloat(), 100f, paint)
+
+                node.title?.let { title ->
+                    paint.color = Color.WHITE
+                    paint.textSize = nodeTextSize
+                    paint.textAlign = Paint.Align.CENTER
+                    paint.getTextBounds(title, 0, title.length, textBounds)
+                    canvas.drawText(
+                        title,
+                        node.xPos.toFloat(),
+                        node.yPos.toFloat() - textBounds.exactCenterY(),
+                        paint
+                    )
+                }
             }
         }
     }
