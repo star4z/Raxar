850a2d51eb9dd3e47fe2a0604de798f26e471db0
Benjamin Phillips <bdphillips013@gmail.com>
2021-02-07 23:53:06 -0500

    Adds logic for 3 rows

--- app/src/main/java/com/example/raxar/view/graph/Graph.kt
+++ app/src/main/java/com/example/raxar/view/graph/Graph.kt
@@ -1,7 +1,10 @@
 package com.example.raxar.view.graph
 
 import com.example.raxar.data.NoteDto
-import kotlin.math.*
+import kotlin.math.PI
+import kotlin.math.asin
+import kotlin.math.cos
+import kotlin.math.sin
 
 open class Graph(notes: List<NoteDto>) : Iterable<Node> {
     private val nodes = notes.map { note -> Node(note) }
@@ -21,6 +24,13 @@ open class Graph(notes: List<NoteDto>) : Iterable<Node> {
             nodeRadiusWithPadding = nodeRadius + value
             field = value
         }
+    var totalRings = 3
+        set(value) {
+            if (value in 0..3) {
+                field = value
+            }
+        }
+
     private var nodeRadiusWithPadding = nodeRadius + padding
     private var width = 1.0 // default val is unused
     private var height = 1.0 // default is unused
@@ -37,14 +47,62 @@ open class Graph(notes: List<NoteDto>) : Iterable<Node> {
         val numberOfPossibleNodesForCircle =
             PI / asin(nodeRadiusWithPadding / (distanceFromOriginYToHeightRatio * height))
         val numberOfNodesToDisplay =
-            floor(numberOfPossibleNodesForCircle * maxAngleFromVertical / PI)
+            (numberOfPossibleNodesForCircle * maxAngleFromVertical / PI).toInt()
+        val angleBetweenNodes = maxAngleFromVertical * 2 / numberOfNodesToDisplay
+
+//        val nodeDeque = ArrayDeque(nodes)
+//
+//        for (ringN in 0 until totalRings) {
+//            val numberOfNodesToPop =
+//                if (ringN == 0) numberOfNodesToDisplay else numberOfNodesToDisplay - 1
+//            val startingAngle =
+//                if (ringN == 0) -maxAngleFromVertical else -maxAngleFromVertical + 0.5 * angleBetweenNodes
+//            for (index in 0 until numberOfNodesToPop) {
+//                nodeDeque.removeLastOrNull()?.let { noteNode ->
+//                    noteNode.state.visible = true
+//                    val theta = angleBetweenNodes * index - startingAngle
+//                    noteNode.xPos =
+//                        (nodeDistanceFromOrigin - nodeRadiusWithPadding) * sin(theta) + origin.xPos
+//                    noteNode.yPos =
+//                        (nodeDistanceFromOrigin - nodeRadiusWithPadding) * cos(theta) + origin.yPos
+//                }
+//            }
+//        }
+//
+//        // Set all remaining nodes as invisible
+//        while (!nodeDeque.isEmpty()) {
+//            val noteNode = nodeDeque.removeLast()
+//            noteNode.state.visible = false
+//        }
 
         nodes.forEachIndexed { index, noteNode ->
-            // this needs to be calculated exactly
-            val theta = maxAngleFromVertical * (2 / numberOfNodesToDisplay * index - 1)
-            noteNode.xPos = nodeDistanceFromOrigin * sin(theta) + origin.xPos
-            noteNode.yPos = nodeDistanceFromOrigin * cos(theta) + origin.yPos
-            noteNode.state.visible = index <= numberOfNodesToDisplay
+            noteNode.state.visible = true
+            when (index) {
+                in 0..numberOfNodesToDisplay -> {
+                    val theta = angleBetweenNodes * index - maxAngleFromVertical
+                    noteNode.xPos = nodeDistanceFromOrigin * sin(theta) + origin.xPos
+                    noteNode.yPos = nodeDistanceFromOrigin * cos(theta) + origin.yPos
+                }
+                in numberOfNodesToDisplay + 1..numberOfNodesToDisplay * 2 -> {
+                    val theta =
+                        angleBetweenNodes * (index - numberOfNodesToDisplay - 1) - maxAngleFromVertical + 0.5 * angleBetweenNodes
+                    noteNode.xPos =
+                        (nodeDistanceFromOrigin - 2 * nodeRadiusWithPadding) * sin(theta) + origin.xPos
+                    noteNode.yPos =
+                        (nodeDistanceFromOrigin - 2 * nodeRadiusWithPadding) * cos(theta) + origin.yPos
+                }
+                in numberOfNodesToDisplay * 2 + 1..numberOfNodesToDisplay * 3 -> {
+                    val theta =
+                        angleBetweenNodes * (index - (numberOfNodesToDisplay * 2 + 1)) - maxAngleFromVertical + 0.5 * angleBetweenNodes
+                    noteNode.xPos =
+                        (nodeDistanceFromOrigin + 2 * nodeRadiusWithPadding) * sin(theta) + origin.xPos
+                    noteNode.yPos =
+                        (nodeDistanceFromOrigin + 2 * nodeRadiusWithPadding) * cos(theta) + origin.yPos
+                }
+                else -> {
+                    noteNode.state.visible = false
+                }
+            }
         }
     }
 
