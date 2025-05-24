080b5005877dc1ac94283dc6b4526119059f2d04
Benjamin Phillips <bdphillips013@gmail.com>
2025-05-24 12:12:43 -0700

    Removes unused classes

--- app/src/main/java/com/example/raxar/MyButton.kt
+++
@@ -1,9 +0,0 @@
-package com.example.raxar
-
-import android.content.Context
-import android.view.View
-import androidx.appcompat.widget.AppCompatButton
-
-class MyButton(context: Context): AppCompatButton(context) {
-    val children = mutableListOf<View>()
-}
--- app/src/main/java/com/example/raxar/MyTextView.kt
+++
@@ -1,9 +0,0 @@
-package com.example.raxar
-
-import android.content.Context
-import android.view.View
-import androidx.appcompat.widget.AppCompatTextView
-
-class MyTextView(context: Context) : AppCompatTextView(context) {
-    val children = mutableListOf<View>()
-}
